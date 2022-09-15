6088235cf9e901e1097d8587babf66665ee248bd
“[Dishant <dishantyadav599@gmail.com>
2022-09-16 00:19:45 +0530

    fixed issues
    Signed-off-by: “[Dishant <dishantyadav599@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">App</a>
+                    <Link className="navbar-brand" to="/">App</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
